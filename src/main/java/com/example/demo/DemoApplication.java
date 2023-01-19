78dbd18984c4866652ff2b7ed2c930f57d9cc910
Alex <lisatthu35@gmail.com>
2023-01-19 15:13:14 +0700

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,31 +7,24 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.boot.autoconfigure.jdbc.*;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = {
+    DataSourceAutoConfiguration.class,
+    DataSourceTransactionManagerAutoConfiguration.class,
+    HibernateJpaAutoConfiguration.class})
 @RestController
 public class DemoApplication {
-
-	@Autowired
-	UserRepository userRepository;
-
+        public static int count = 0;
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
-	@GetMapping("count-users")
+	@GetMapping("count")
 	public String getCountUsers(){
-		long numberUsers = userRepository.count();
-		return "Number of users = "+numberUsers;
-	}
-
-	@GetMapping("add-user")
-	public String addUser(){
-		User user = new User();
-		user.setFirstName("Render");
-		user.setLastName("Spring Boot");
-		userRepository.save(user);
-		return "User added successfully";
+		count++;
+		return "Number of users = "+ count;
 	}
 
 }
